c6096f99813f839276dd6ebccbc3b58ff9bd4952
durjoy1971 <durjoybarua1971@gmail.com>
2024-10-30 17:03:33 +0600

    Basic CRUD with Basic Adonis JS + Controller

--- start/routes.ts
+++ start/routes.ts
@@ -1,6 +1,7 @@
+const CrudsController = () => import('#controllers/cruds_controller')
 import router from '@adonisjs/core/services/router'
 
-let userDatabase = [
+export const userDatabase = [
   { id: 1, name: 'John Doe' },
   { id: 2, name: 'Jane Doe' },
   { id: 3, name: 'Doe John' },
@@ -8,37 +9,12 @@ let userDatabase = [
 
 router.get('/', () => 'Hello World')
 
-router.get('/users', ({ response }) => response.status(200).json(userDatabase))
+router.get('/users', [CrudsController, 'allUser'])
 
-router.get('/users/:id', ({ params, response }) => {
-  const user = userDatabase.find((userID) => userID.id === Number(params.id))
-  return user
-    ? response.status(200).json(user)
-    : response.status(404).json({ message: 'User not found' })
-})
+router.get('/users/find/:id', [CrudsController, 'findUser'])
 
-router.post('/users/update/:id', ({ params, request, response }) => {
-  const { name } = request.only(['name'])
-  const userIndex = userDatabase.findIndex((userID) => userID.id === Number(params.id))
-  if (userIndex === -1) return response.status(404).json({ message: 'User not found' })
-  userDatabase[userIndex] = { ...userDatabase[userIndex], name }
-  return response.status(200).json(userDatabase[userIndex])
-})
+router.post('/users/update/:id', [CrudsController, 'updateUser'])
 
-router.post('/users/delete/:id', ({ params, response }) => {
-  const userIndex = userDatabase.findIndex((userID) => userID.id === Number(params.id))
-  if (userIndex === -1) return response.status(404).json({ message: 'User not found' })
-  userDatabase.splice(userIndex, 1)
-  return response.status(200).json({ message: 'User deleted successfully' })
-})
+router.post('/users/delete/:id', [CrudsController, 'deleteUser'])
 
-router.post('/users/create', ({ request, response }) => {
-  const { name } = request.only(['name'])
-  let id = 1
-  while (true) {
-    if (!userDatabase.find((userID) => userID.id === id)) break
-    id++
-  }
-  userDatabase.push({ id, name })
-  return response.status(201).json(userDatabase)
-})
+router.post('/users/create', [CrudsController, 'createUser'])
